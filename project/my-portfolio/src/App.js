16097241b88dac741fce32ee8650064c07c56c18
Veronica Maya Santi <veronicamayasanti@gmail.com>
2024-05-02 18:03:30 +0700

    make header

--- project/my-portfolio/src/App.js
+++ project/my-portfolio/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import ReactGA from "react-ga"
 import './App.css';
+import { Component } from 'react';
+import $ from "jquery";
+import Header from './Components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      foo: "bar",
+      resumeData: {}
+    };
+    ReactGA.initialize("UA-110570651-1");
+    ReactGA.pageview(window.location.pathname);
+  }
+
+  getResumeData() {
+    $.ajax({
+      url: "./resumeData.json",
+      dataType: "json",
+      cache: false,
+      success: function(data) {
+        this.setState({
+          resumeData: data
+        });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.log(err);
+        alert(err)
+      }
+    })
+  }
+  componentDidMount(){
+    this.getResumeData();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header data={this.state.resumeData.main}/>
+      </div>
+    )
+  }
 }
 
 export default App;
