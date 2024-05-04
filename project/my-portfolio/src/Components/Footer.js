875e2730235b347746a7821f1fa7f0db75edef5d
Veronica Maya Santi <veronicamayasanti@gmail.com>
2024-05-04 16:15:27 +0700

    finish

--- project/my-portfolio/src/Components/Footer.js
+++ project/my-portfolio/src/Components/Footer.js
@@ -20,12 +20,12 @@ class Footer extends Component {
                 <div className="row">
                     <Fade bottom>
                         <div className="twelve columns">
-                            <ul className="social-link">{networks}</ul>
+                            <ul className="social-links">{networks}</ul>
                             <ul className="copyright">
                                 <li>&copy; Copyright 2024 veronica maya santi</li>
                                 <li>
                                     Design by {" "}
-                                    <a title="styleshout" href="http://www.styleshout.com/">
+                                    <a title="Styleshout" href="http://www.styleshout.com/">
                                         Styleshout
                                     </a>
                                 </li>
