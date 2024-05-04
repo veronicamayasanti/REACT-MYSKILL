c821a2a3e2cc4c612d34e313ae9f99a29e2a823b
Veronica Maya Santi <veronicamayasanti@gmail.com>
2024-05-04 13:00:47 +0700

    Inserting Project Portfolio

--- project/my-portfolio/src/App.js
+++ project/my-portfolio/src/App.js
@@ -6,6 +6,7 @@ import Header from './Components/Header';
 import About from './Components/About';
 import Contact from "./Components/Contact";
 import Resume from "./Components/Resume";
+import Portfolio from "./Components/Portfolio";
 
 class App extends Component {
   constructor(props) {
@@ -44,6 +45,7 @@ class App extends Component {
         <Header data={this.state.resumeData.main}/>
         <About data={this.state.resumeData.main} />
         <Resume data = {this.state.resumeData.resume}/>
+        <Portfolio data = {this.state.resumeData.portfolio}/>
         <Contact data = {this.state.resumeData.main} />
       </div>
     )
